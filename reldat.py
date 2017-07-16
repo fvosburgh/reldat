d590f75fd9c07bda42682f00d775085e32e1ae14
Forester Vosburgh <fvosburgh3@gmail.com>
2017-07-15 20:04:17 -0400

    non threaded branch, start on protocol specifics

--- reldat.py
+++ reldat.py
@@ -1,5 +1,5 @@
 import socket
-import md5
+import hashlib
 import pickle
 from threading import Thread, Lock
 from io import BytesIO
@@ -9,11 +9,7 @@ class Reldat:
     MAX_PAYLOAD_SIZE = 1000
     WINDOW_SIZE = 0
 
-    recv_buff = BytesIO()
-
-    # Since we're writing to this buffer in another thread, we need a mutex
-    recv_buff_mutex = Lock()
-
+    SYN_SIZE = len()
 
     # pickle delimmeter for each dump is \x80\x04\x95#
     # the # after \x95 denotes an object rather than a primitive
@@ -28,38 +24,42 @@ class Reldat:
     def set_window(window_size):
         WINDOW_SIZE = window_size
 
-    # This function is a wrapper for the receving thread start function for
-    # the socket. This should only be called once.
-    def listen(socket):
-        recv_window = 1048
-        socket.start_receive(recv_window, recv_buff, recv_buff_mutex)
+    # create header
+    def header(seq_num, ack_num):
+        header = PacketHeader()
+        header.seq_num = seq_num
+        header.ack_num = ack_num
+        return header
 
-    def get_data():
-        while recv_buff.getvalue() is 0:
-            pass
-        #TODO parse the buffer into packets to be deserialized
-        # working regex (?<!\\x)(\d{2,3}\.){3}\d{1,3}
-        # has issues in interpreter, dont know why yet
+    # create syn packet
+    def syn(seq_num, ack_num):
+        header = header(seq_num, ack_num)
+        header.syn = 1
+        return ReldatPacket(header)
 
-        # Use pickle protocol 0 for ascii encoding, then convert to string
-        # before regexing?
+    # create ack packet
+    def ack(seq_num, ack_num):
+        header = header(seq_num, ack_num)
+        header.ack = 1
+        return ReldatPacket(header)
 
+    # create data packet
+    def data(seq_num, ack_num, fin = 0, data):
+        header = header(seq_num, ack_num)
+        header.fin = fin
+        return ReldatPacket(header, data)
 
+    # listen for and accept an incoming connection and complete the handshake
+    def accept(socket):
+        addr, data = socket.receive(247)
 
-
+class Connection:
+    def __init__(self, addr = None):
+        self.addr = addr
 
 class ReldatSocket:
 
     #TODO remove erronious prints when done testing
-
-    #TODO do we need threading?
-    # Use this mutex to control when to throttle the listening threading
-    throttle_mutex = Lock()
-
-    # Use this mutex to ensure that we're not listening when we're trying to
-    # send on the socket and vice versa
-    socket_mutex = Lock()
-
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -73,12 +73,6 @@ class ReldatSocket:
         # will demux the incoming packets and determine which client to
         # respond to
         self.source_addr = None
-
-        # This determines whether or not we need to stop receiving so that the
-        # protocol can finish processing the recv_buffer
-        throttle_mutex.acquire()
-        self.throttle = False
-        throttle_mutex.release()
 
         print("Socket initialized")
 
@@ -99,33 +93,12 @@ class ReldatSocket:
         self.state = "closed"
 
     def send(self, packet, dest_addr):
-
-        #NOTE
-        # some concern about deadlock here. What if we're stuck listening
-        # because nothing is coming in, but we want to send data still?
-        # Can we rely on a timeout of some sort for listening?
-        socket_mutex.acquire()
-        self._socket.sendto(packet.serialize(), dest_addr)
-        socket_mutex.release()
-
-    def throttle(self):
-        throttle_mutex.acquire()
-        self.throttle = True
-        throttle_mutex.release()
-
-    def remove_throttle(self):
-        throttle_mutex.acquire()
-        self.throttle = False
-        throttle_mutex.release()
-
-    # This function spawns a thread that continuously receives data
-    # on the socket. The thread runs as a daemon so that it is non blocking
-    # on program termination
-    def start_receive(recv_window_size, recv_buffer, recv_buffer_mutex):
-        t = Thread(target = receive, args = (recv_window_size, recv_buffer,
-                                             recv_buffer_mutex))
-        t.setDaemon(True)
-        t.start()
+        try:
+            self._socket.sendto(packet.serialize(), dest_addr)
+            return 0
+        except Exception as e:
+            print("Socket error while sending: ", e)
+            return -1
 
     #TODO better timeout calculation
     # This needs to be a threaded function since we will always be Listening
@@ -134,52 +107,28 @@ class ReldatSocket:
     #TODO
     # How should we handle a changing window size? Is it appropriate to even
     # use window size here?
-    def receive(self, recv_window_size, recv_buffer, recv_buffer_mutex):
-        while True and not self.throttle:
+    def receive(self, recv_size):
+        retval = 0
+        while True:
             try:
-                socket_mutex.acquire()
-                data, addr = self._socket.recvfrom(int(recv_window_size))
-                socket_mutex.release()
-                recv_buffer_mutex.acquire()
-                recv_buffer.write(bytes(addr, encoding='ascii') + data)
-                recv_buffer_mutex.release()
+                data, addr = self._socket.recvfrom(int(recv_size))
+                retval = (addr, pickle.loads(data, protocol=pickle.HIGHEST_PROTOCOL))
+                break
             except Exception as e:
                 print("Socket error while receiving: ", e)
-
-
+        return retval
 
 class ReldatPacket:
-    class PacketHeader:
-        def __init__(self):
-            self.source_port = 0
-            self.dest_port = 0
-            self.seq_num = 0
-            self.ack_num = 0
-            self.syn = 0
-            self.ack = 0
-            self.fin = 0
-            self.window = 0
-            self.checksum = 0
-            self.payload_length = 0
 
-        # We stringify the header to have data for the packet checksum
-        def to_string(self):
-            retval = ""
-            for attr, value in self.__dict__.items():
-                if attr != "checksum":
-                    retval += value
-            return retval
-
-        # END PACKETHEADER CLASS
-
-    def __init__(self, data = None):
-        self.header = PacketHeader()
+    def __init__(self, header = None, data = None):
+        self.header = header or PacketHeader()
         self.data = data or ' '
         self.header.payload_length = len(self.data)
-        self.header.checksum = checksum()
+        self.header.checksum = self.checksum()
 
     def checksum(self):
-        return md5.new(self.header.to_string + self.data).digest()
+        s = self.header.to_string().encode('utf-8') + self.data.encode('utf-8')
+        return hashlib.md5(s).hexdigest()
 
     def verify(self):
         # we don't include the checksum in the header when computing the
@@ -192,3 +141,28 @@ class ReldatPacket:
         # We use the HIGHEST_PROTOCOL to convert to a byte stream rather
         # than a string.
         return pickle.dumps(self, protocol=pickle.HIGHEST_PROTOCOL)
+
+class PacketHeader:
+    def __init__(self):
+        self.source_port = 0
+        self.dest_port = 0
+
+        # sequencing
+        self.seq_num = 0
+        self.ack_num = 0
+
+        # control
+        self.syn = 0
+        self.ack = 0
+        self.fin = 0
+
+        self.window = 0
+        self.checksum = 0
+        self.payload_length = 0
+
+    # We stringify the header to have data for the packet checksum
+    def to_string(self):
+        retval = ""
+        for value in self.__dict__.values():
+            retval += str(value)
+        return retval
